23b4b78996652e6871bf9baa44e3a2c1a8371868
Bekzat-frontend <mamateminovbekzat793@gmail.com>
2025-02-21 18:40:37 +0700

    Fixed Axios timer issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import Home from "./components/Home";
+import NewHome from "./components/NewHome";
 function App() {
   return (
     <div>
       <Home />
+      <NewHome />
     </div>
   );
 }
